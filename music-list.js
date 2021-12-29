397cac6bb0c8b41d5fb18c7c59574bfb1867650b
SANO Bruce <87866704+Umwijima@users.noreply.github.com>
2021-12-29 10:33:27 +0200

    Create music-list.js

---
+++ music-list.js
@@ -0,0 +1,62 @@
+let allMusic = [
+  {
+    name: "Amashu",
+    artist: "Chriss Eazy",
+    img: "chrisseazy",
+    src: "amashu"
+  },
+  {
+    name: "Amata",
+    artist: "Social Mula ft Phil Peter",
+    img: "amata",
+    src: "amata"
+  },
+  {
+    name: "Bimpame",
+    artist: "Phil Peter ft Marina",
+    img: "bimpame",
+    src: "bimpame"
+  },
+  {
+    name: "Birenze",
+    artist: "Juno Kizigenza",
+    img: "juno",
+    src: "birenze"
+  },
+  {
+    name: "Fasta",
+    artist: "Chriss Eazy",
+    img: "chrisseazy",
+    src: "fasta"
+  },
+  {
+    name: "Ishyano",
+    artist: "Niyo Bosco",
+    img: "niyobosco",
+    src: "ishyano"
+  },
+  {
+    name: "Iyallah",
+    artist: "Okamma",
+    img: "okamma",
+    src: "iyallah"
+  },
+  {
+    name: "Mutuale",
+    artist: "Bruce Melody ft Nel Ngabo",
+    img: "mutuale",
+    src: "mutuale"
+  },
+  {
+    name: "Say Ma Name",
+    artist: "Kenny Sol",
+    img: "kennysol",
+    src: "saymaname"
+  },
+  {
+    name: "Shumuleta",
+    artist: "Platini P",
+    img: "platini",
+    src: "shumuleta"
+  },
+  ]
