a9785e3a4d84915505ff633c0e341cc407ddb768
SANO Bruce <87866704+Umwijima@users.noreply.github.com>
2021-12-29 11:18:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ window.addEventListener("load", ()=>{
 function loadMusic(indexNumb){
   musicName.innerText = allMusic[indexNumb - 1].name;
   musicArtist.innerText = allMusic[indexNumb - 1].artist;
-  musicImg.src = `images/${allMusic[indexNumb - 1].src}.jpg`;
-  mainAudio.src = `songs/${allMusic[indexNumb - 1].src}.mp3`;
+  musicImg.src = `${allMusic[indexNumb - 1].src}.jpg`;
+  mainAudio.src = `${allMusic[indexNumb - 1].src}.mp3`;
 }
 
 //play music function
